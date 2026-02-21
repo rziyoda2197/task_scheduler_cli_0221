239f63bfc257d095c5300f08e726473a1af899a6
rziyoda2197 <rziyoda2197@gmail.com>
2026-02-21 21:36:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+seconds = int(input("Necha sekunddan keyin ishga tushsin: "))
+task = input("Task nomi: ")
+
+print("Kutilyapti...")
+time.sleep(seconds)
+
+print("Task bajarildi:", task)
